37664111ca0fb47a38fef5ebc3f98a254043c9a1
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-08 14:44:38 -0800

    Adds handleIndexBookings function to BookingsIndex with axios request from backend to grab all bookings by signed-in user

--- src/BookingsIndex.jsx
+++ src/BookingsIndex.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Modal } from "./Modal";
+
 export function BookingsIndex() {
+  const [bookings, setBookings] = useState([]);
+  const [isBookingVisible, setIsBookingVisible] = useState(false);
+  const [currentBooking, setCurrentBooking] = useState([]);
+
+  const handleIndexBookings = () => {
+    axios.get("http://localhost:3000/bookings.json").then((response) => {
+      console.log(response.data);
+      setBookings(response.data);
+    });
+  };
+
+  const handleShowBooking = (booking) => {
+    setIsBookingVisible(true);
+    setCurrentBooking(booking);
+  };
+
+  const handleHideBooking = () => {
+    setIsBookingVisible(false);
+  };
+
+  useEffect(handleIndexBookings, []);
+
   return (
     <div>
       <h1>My Bookings!</h1>
+      <Modal show={isBookingVisible} onClose={handleHideBooking}>
+        {/* <BookingShow booking={currentBooking} /> */}
+      </Modal>
     </div>
   );
 }
