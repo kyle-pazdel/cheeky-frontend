92a39be9debe42ef6bf2da632162e0afed072f58
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-09 12:35:29 -0800

    Removes some event details from Bookings Index Page

--- src/BookingsIndex.jsx
+++ src/BookingsIndex.jsx
@@ -23,20 +23,9 @@ export function BookingsIndex() {
           <h2>
             {booking.event_name} with {booking.performer_name}
           </h2>
-          <p>Event Type: {booking.event_type}</p>
-          <p>Hourly Rate: {booking.performer_rate}</p>
-          <p>Total: {booking.total}</p>
           <p>
-            Start Time: <DayJs format="MMMM D, YYYY">{booking.start_time}</DayJs> at
-            <DayJs format="h:mm A">{booking.start_time}</DayJs> – End Time:
-            <DayJs format="MMMM D, YYYY">{booking.end_time}</DayJs> at <DayJs format="h:mm A">{booking.end_time}</DayJs>
+            Date: <DayJs format="MMMM D, YYYY">{booking.start_time}</DayJs>
           </p>
-          <p>
-            Location: {booking.address} {booking.city} {booking.state} {booking.postal_code}
-          </p>
-          <small>
-            Contact: {booking.performer_name} {booking.performer_email} {booking.performer_phone_number}
-          </small>
           <div>
             <Link to={`/bookings/${booking.id}`}>See Booking Details</Link>
           </div>
