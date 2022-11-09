fa0cf33123448f63322ec4b24fcb48024d77ceb9
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-08 17:07:40 -0800

    Passes performer object and userId throuhg Link when 'Book Now' is clicked from PerformersShow page to BookingsNew page

--- src/BookingsNew.jsx
+++ src/BookingsNew.jsx
@@ -1,7 +1,21 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 export function BookingsNew() {
+  const location = useLocation();
+  // const { performer } = location.state;
+  const performer = location.state.performer;
+  const userId = location.state.userId;
+  const showPerformer = () => {
+    console.log(performer);
+    console.log(userId);
+  };
+
+  useEffect(showPerformer);
   return (
     <div>
-      <h1>Form to Go Here.</h1>
+      <h1>Book with {performer.name}</h1>
+      <p>Your user id # is {userId}</p>
     </div>
   );
 }
--- src/PerformersShow.jsx
+++ src/PerformersShow.jsx
@@ -1,4 +1,10 @@
+import { Link } from "react-router-dom";
+import { BookingsNew } from "./BookingsNew";
+
 export function PerformersShow(props) {
+  const userId = localStorage.getItem("user_id");
+  const performer = props.performer;
+
   return (
     <div>
       <h4>{props.performer.name}</h4>
@@ -14,6 +20,9 @@ export function PerformersShow(props) {
       <p>{props.performer.email}</p>
       <p>@{props.performer.twitter_handle} on Twitter</p>
       <p>{props.performer.instagram_handle} on Instagram</p>
+      <Link to="/book" state={{ performer, userId }}>
+        Book Now
+      </Link>
     </div>
   );
 }
