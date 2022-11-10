efff169e4303258567d1dde89e05b656b8e7072e
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-10 07:33:49 -0800

    Installs styled-components lib and imports into DatePickerComponent, adds date selection parameters and dateFormat to DatePickerComponent

--- src/DatePickerComponent.jsx
+++ src/DatePickerComponent.jsx
@@ -1,26 +1,46 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import styled from "styled-components";
 
 export function DatePickerComponent() {
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
   console.log(startDate);
-  const [endDate, setEndDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+
   return (
     <div>
       Start Date:
       <DatePicker
+        isClearable
+        filterDate={(d) => {
+          return new Date() <= d;
+        }}
+        placeholderText="Select Start Date"
         selected={startDate}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
         onChange={(date) => setStartDate(date)}
         showTimeSelect
-        dateFormat="Pp"
+        dateFormat="MMMM d, yyyy h:mmaa"
+        // dateFormat="Pp"
         allowSameDay={true}
         name="start_time"
       />
       End Date:
       <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
+        isClearable
+        filterDate={(d) => {
+          return new Date() <= d;
+        }}
+        placeholderText="Select End Date"
+        selected={endDate}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        mindDate={startDate}
+        onChange={(date) => setEndDate(date)}
         showTimeSelect
         dateFormat="Pp"
         allowSameDay={true}
