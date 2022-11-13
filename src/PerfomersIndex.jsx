22ce33bf9e967e4ea4bba2c800770aaa94e22923
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-13 13:50:39 -0800

    Adds function to calculate gig range and display in hours or minutes accordingly

--- src/PerfomersIndex.jsx
+++ src/PerfomersIndex.jsx
@@ -6,14 +6,24 @@ export function PerformersIndex(props) {
       {props.performers.map((performer) => (
         <div key={performer.id} className="card col-2" style={{ width: "18rem" }}>
           <div>
-            <img src={performer.profile_image?.image_url} alt={`image of ${performer.name}`} className="card-img" />
+            <img
+              src={performer.profile_image?.image_url}
+              alt={`image of ${performer.name}`}
+              className="card-img card-img-top"
+            />
           </div>
           <div className="card-img h-100 d-flex flex-column justify-content-end">
             <h5 className="card-title">{performer.name}</h5>
-            <small className="card-text">
-              {performer.city}, {performer.state}
-            </small>
-            <Link to={`/performers/${performer.id}`} className="btn btn-primary">
+            {performer.city !== "" && performer.state !== "" ? (
+              <small className="card-text">
+                {performer.city}, {performer.state}
+              </small>
+            ) : (
+              <small className="card-text">
+                {performer.city} {performer.state}
+              </small>
+            )}
+            <Link to={`/performers/${performer.id}`} className="btn btn-dark">
               More Info
             </Link>
           </div>
--- src/PerformersShow.jsx
+++ src/PerformersShow.jsx
@@ -5,6 +5,7 @@ import { BookingsNew } from "./BookingsNew";
 import { Modal } from "./Modal";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Link } from "react-router-dom";
 
 export function PerformersShow() {
   const params = useParams();
@@ -12,12 +13,16 @@ export function PerformersShow() {
   const [performer, setPerformer] = useState({});
   const [isBookingFormVisible, setIsBookingFormVisible] = useState(false);
   const [posts, setPosts] = useState([]);
+  const [shortestGig, setShortestGig] = useState(0);
+  const [longestGig, setLongestGig] = useState(0);
 
   const handleShowPerformer = () => {
     axios.get(`/performers/${params.id}.json`).then((response) => {
       console.log(response.data);
       setPerformer(response.data);
       setPosts(response.data.posts);
+      setShortestGig(response.data.shortest_gig);
+      setLongestGig(response.data.longest_gig);
       console.log(response.data.posts);
     });
   };
@@ -45,29 +50,43 @@ export function PerformersShow() {
       });
   };
 
+  const formatTime = (time) => {
+    console.log(time);
+    if (time >= 60) {
+      const quotient = time / 60;
+      console.log(quotient);
+      return Math.round(quotient) + " hr";
+    } else {
+      return time + " min";
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <img
-          src={performer.profile_image?.image_url}
-          alt={`photo of ${performer.name}`}
-          className="thumbnail-profile-image rounded-circle"
-        />
-        <h4>{performer.name}</h4>
+    <div className="row">
+      <div className="col-3">
+        <div className="fw-bold">
+          <img
+            src={performer.profile_image?.image_url}
+            alt={`photo of ${performer.name}`}
+            className="thumbnail-profile-image rounded-circle"
+          />
+          <p className="fs-3">{performer.name}</p>
+        </div>
+        <div className="fw-semibold mt-6">
+          <p>
+            {performer.city}, {performer.state}
+          </p>
+        </div>
+        <p>
+          {formatTime(shortestGig)} – {formatTime(longestGig)}
+        </p>
+        <p>performance type: {performer.performance_type}</p>
+        <p>hourly rate: {performer.rate}</p>
+        <p>{performer.bio}</p>
+        <p>@{performer.twitter_handle} on Twitter</p>
+        <p>{performer.instagram_handle} on Instagram</p>
       </div>
-      <p>
-        {performer.city}, {performer.state}
-      </p>
-      <p>{performer.bio}</p>
-      <p>
-        gig range: {performer.shortest_gig}min – {performer.longest_gig}min
-      </p>
-      <p>performance type: {performer.performance_type}</p>
-      <p>hourly rate: {performer.rate}</p>
-      <p>{performer.email}</p>
-      <p>@{performer.twitter_handle} on Twitter</p>
-      <p>{performer.instagram_handle} on Instagram</p>
-      <div>
+      <div className="col-9">
         <Carousel autoPlay={true} showArrows={true} showThumbs={true}>
           {posts?.map((post) => (
             <div>
@@ -79,7 +98,20 @@ export function PerformersShow() {
           ))}
         </Carousel>
       </div>
-      {localStorage.jwt !== undefined ? <button onClick={() => handleShowBookingForm()}>Book Now</button> : null}
+      {localStorage.jwt !== undefined ? (
+        <button onClick={() => handleShowBookingForm()} className="btn btn-dark">
+          Book Now
+        </button>
+      ) : (
+        <div className="row">
+          <Link to="/signup" className="col-12 btn btn-dark">
+            Sign Up to Book Now
+          </Link>
+          <Link to="/login" class="col-12 link-dark text-wrap fst-italic">
+            Already a member? Click here to sign in.
+          </Link>
+        </div>
+      )}
       <Modal show={isBookingFormVisible} onClose={handleHideBookingForm}>
         <BookingsNew errors={errors} performer={performer} onCreateBooking={handleCreateBooking} />
       </Modal>
