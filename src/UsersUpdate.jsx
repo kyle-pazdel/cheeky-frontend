a889e713f0fe42bd9150e9102e7bbdc795327101
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-14 08:30:20 -0800

    Adds styling to UsersUpdate form

--- src/UsersUpdate.jsx
+++ src/UsersUpdate.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export function UsersUpdate(props) {
+  const [firstName, setFirstName] = useState(props.user.first_name);
+  const [lastName, setLastName] = useState(props.user.last_name);
+  const [email, setEmail] = useState(props.user.email);
+  const [phoneNumber, setPhoneNumber] = useState(props.user.phone_number);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const params = new FormData(event.target);
+    const params = {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      phone_number: phoneNumber,
+    };
     props.onHideUserForm();
     props.onUpdateUser(props.user.id, params, () => event.target.reset());
   };
@@ -12,19 +24,61 @@ export function UsersUpdate(props) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div>
-          First Name: <input defaultValue={props.user.first_name} name="first_name" type="text" />
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
+            First Name
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-sm"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+          ></input>
         </div>
-        <div>
-          Last Name: <input defaultValue={props.user.last_name} name="last_name" type="text" />
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
+            Last Name
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-sm"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+          ></input>
         </div>
-        <div>
-          Email: <input defaultValue={props.user.email} name="email" type="text" />
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
+            Email
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-sm"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          ></input>
         </div>
-        <div>
-          Phone Number: <input defaultValue={props.user.phone_number} name="phone_number" type="text" />
+        <div className="input-group input-group-sm mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-sm">
+            Phone Number
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-sm"
+            value={phoneNumber}
+            onChange={(event) => setPhoneNumber(event.target.value)}
+          ></input>
         </div>
-        <button type="submit">Update Account Details</button>
+        <button className="btn btn-dark" type="submit">
+          Update Account Details
+        </button>
       </form>
       {props.errors !== undefined ? (
         <ul>
@@ -34,7 +88,9 @@ export function UsersUpdate(props) {
         </ul>
       ) : null}
       <div>
-        <button onClick={handleClick}>Delete Account</button>
+        <button className="btn btn-outline-success btn-sm" onClick={handleClick}>
+          Delete Account
+        </button>
       </div>
     </div>
   );
