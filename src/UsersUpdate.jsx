2ce5162c11e665daefd9ee4af96651ce8b765631
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-14 14:53:34 -0800

    Adds delete popup to UsersUpdate

--- src/UsersUpdate.jsx
+++ src/UsersUpdate.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
+import { Modal } from "./Modal";
 
 export function UsersUpdate(props) {
   const [firstName, setFirstName] = useState(props.user.first_name);
   const [lastName, setLastName] = useState(props.user.last_name);
   const [email, setEmail] = useState(props.user.email);
   const [phoneNumber, setPhoneNumber] = useState(props.user.phone_number);
+  const [isAccountDeleteVisible, setIsAccountDeleteVisible] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -18,6 +20,14 @@ export function UsersUpdate(props) {
     };
     props.onHideUserForm();
     props.onUpdateUser(props.user.id, params, () => event.target.reset());
+  };
+
+  const handleShowAccountDelete = () => {
+    setIsAccountDeleteVisible(true);
+  };
+
+  const handleHideAccountDelete = () => {
+    setIsAccountDeleteVisible(false);
   };
 
   const handleClick = () => {
@@ -72,20 +82,6 @@ export function UsersUpdate(props) {
           </span> */}
           <PhoneInput placeholder="Enter phone number" value={phoneNumber} onChange={setPhoneNumber} />
         </div>
-
-        {/* <div className="input-group input-group-sm mb-3">
-          <span className="input-group-text" id="inputGroup-sizing-sm">
-            Phone Number
-          </span>
-          <input
-            type="text"
-            className="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-sm"
-            value={phoneNumber}
-            onChange={(event) => setPhoneNumber(event.target.value)}
-          ></input>
-        </div> */}
         <button className="btn btn-dark" type="submit">
           Update Account Details
         </button>
@@ -98,9 +94,20 @@ export function UsersUpdate(props) {
         </ul>
       ) : null}
       <div>
-        <button className="btn btn-outline-success btn-sm" onClick={handleClick}>
+        {/* <button className="btn btn-outline-success btn-sm" onClick={handleClick}>
+          Delete Account
+        </button> */}
+        <button className="btn btn-outline-success btn-sm" onClick={handleShowAccountDelete}>
           Delete Account
         </button>
+        <Modal show={isAccountDeleteVisible} onClose={handleHideAccountDelete}>
+          <div className="card m-4">
+            <p className="cardTitle">Are you sure you want to cancel?</p>
+            <button className="btn btn-outline-success" onClick={handleClick}>
+              Yes, proceed.
+            </button>
+          </div>
+        </Modal>
       </div>
     </div>
   );
