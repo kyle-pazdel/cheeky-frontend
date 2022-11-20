e4d730e8e3d62ae1bd1a49616580cab3bf12b611
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-20 12:37:36 -0800

    Adds new navbar design with custom function for transparency/background color on home page

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,79 +1,141 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
+import { useState } from "react";
 
 export function Header() {
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () => {
+    if (location.pathname !== "/") {
+      setColorchange(true);
+    } else if (window.scrollY >= 80 && location.pathname === "/") {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
   return (
-    <header>
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">
-            Cheeky
-          </NavLink>
+    <>
+      {/* <header>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+          <div className="container-fluid">
+            <NavLink className="navbar-brand" to="/">
+              Cheeky
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                {localStorage.jwt === undefined ? (
+                  <>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/login">
+                        Login
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/signup">
+                        Signup
+                      </NavLink>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <LogoutLink />
+                    </li>
+                    <li className="nav-item dropdown">
+                      <a
+                        className="nav-link dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        My Account
+                      </a>
+                      <ul className="dropdown-menu dropdown-menu-dark">
+                        <li>
+                          <NavLink className="dropdown-item" to="/me">
+                            My Profile
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink className="dropdown-item" to="/my-bookings">
+                            My Bookings
+                          </NavLink>
+                        </li>
+                      </ul>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header> */}
+      <nav
+        className={
+          colorChange
+            ? "navbar colorChange navbar-expand-lg navbar-dark fixed-top py-3"
+            : "navbar navbar-expand-lg navbar-dark fixed-top py-3"
+        }
+      >
+        <div className="container px-4 px-lg-5">
+          <a className="navbar-brand" href="#page-top">
+            Start Bootstrap
+          </a>
           <button
-            className="navbar-toggler"
+            className="navbar-toggler navbar-toggler-right"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
+            data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto my-2 my-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link" to="/">
-                  Home
-                </NavLink>
+                <a className="nav-link" href="#about">
+                  About
+                </a>
               </li>
-              {localStorage.jwt === undefined ? (
-                <>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/login">
-                      Login
-                    </NavLink>
-                  </li>
-                  <li className="nav-item">
-                    <NavLink className="nav-link" to="/signup">
-                      Signup
-                    </NavLink>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className="nav-item">
-                    <LogoutLink />
-                  </li>
-                  <li className="nav-item dropdown">
-                    <a
-                      className="nav-link dropdown-toggle"
-                      href="#"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      My Account
-                    </a>
-                    <ul className="dropdown-menu dropdown-menu-dark">
-                      <li>
-                        <NavLink className="dropdown-item" to="/me">
-                          My Profile
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink className="dropdown-item" to="/my-bookings">
-                          My Bookings
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </li>
-                </>
-              )}
+              <li className="nav-item">
+                <a className="nav-link" href="#services">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">
+                  Contact
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
-    </header>
+    </>
   );
 }
