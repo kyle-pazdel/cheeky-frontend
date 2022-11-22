bb3ae85516111c82221b26d536d3613cdd9859ca
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-22 10:41:45 -0800

    Adds styles to fade in navbar background color in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-// import { NavLink, useLocation } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import { Header } from "./Header";
@@ -19,20 +17,6 @@ import { PaymentProcessing } from "./PaymentProcessing";
 
 function App() {
   axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
-
-  // const [colorChange, setColorchange] = useState(false);
-
-  // const changeNavbarColor = () => {
-  //   if (location.pathname !== "/") {
-  //     setColorchange(true);
-  //   } else if (window.scrollY >= 924 && location.pathname === "/") {
-  //     setColorchange(true);
-  //   } else {
-  //     setColorchange(false);
-  //   }
-  // };
-
-  // window.addEventListener("scroll", changeNavbarColor);
 
   return (
     <BrowserRouter>
