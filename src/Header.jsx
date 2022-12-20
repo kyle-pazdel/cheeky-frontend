e011d1fb99fa99a458e9425496398d4918e2664c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-20 12:39:37 -0800

    Removes commented code in Header and removes scroll position from header

--- src/Header.jsx
+++ src/Header.jsx
@@ -19,78 +19,6 @@ export function Header(props) {
 
   return (
     <>
-      {/* <header>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              Cheeky
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                    Home
-                  </Link>
-                </li>
-                {localStorage.jwt === undefined ? (
-                  <>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/login">
-                        Login
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/signup">
-                        Signup
-                      </Link>
-                    </li>
-                  </>
-                ) : (
-                  <>
-                    <li className="nav-item">
-                      <LogoutLink />
-                    </li>
-                    <li className="nav-item dropdown">
-                      <a
-                        className="nav-link dropdown-toggle"
-                        href="#"
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        My Account
-                      </a>
-                      <ul className="dropdown-menu dropdown-menu-dark">
-                        <li>
-                          <Link className="dropdown-item" to="/me">
-                            My Profile
-                          </Link>
-                        </li>
-                        <li>
-                          <Link className="dropdown-item" to="/my-events">
-                            My Bookings
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                  </>
-                )}
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header> */}
       <nav
         className={
           props.colorChange
@@ -100,7 +28,7 @@ export function Header(props) {
       >
         <div className="container px-4 px-lg-5">
           <a className="navbar-brand" href="#page-top">
-            {scrollPosition}
+            Cheeky
           </a>
           <button
             className="navbar-toggler navbar-toggler-right"
