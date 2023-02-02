6e074db105d1869193c7edbf4abe5873d9abe23d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-02-02 12:54:15 -0800

    Adds styling to Performers Bookings

--- src/PerformersBookings.jsx
+++ src/PerformersBookings.jsx
@@ -30,31 +30,30 @@ export function PerformersBookings() {
     <div className="top-buff">
       <p className="fs-2 fw-semibold">{performer.name}'s Bookings</p>
       {bookings?.map((booking) => (
-        <div key={booking.id}>
-          <div className="card shadow mb-4">
-            <h5 className="card-header">{booking.event_name}</h5>
-            <div className="card-body">
-              <h5 className="card-title">{formatTime(booking.start_time)}</h5>
-              <div className="card-text mt-4">
-                <p> {booking.address}</p>
-                <p>
-                  {booking.city}, {booking.state} {booking.postal_code}
-                </p>
+        <div key={booking.id} className="mb-4 card shadow">
+          <div className="row justify-content-md-center container text-center">
+            <h5 className="card-header pb-4 mb-3">{booking.event_name}</h5>
+            <div className="me-1 col-6">
+              <div className="card-body">
+                <h5 className="card-title">{formatTime(booking.start_time)}</h5>
+                <div className="card-text mt-4">
+                  <p> {booking.address}</p>
+                  <p>
+                    {booking.city}, {booking.state} {booking.postal_code}
+                  </p>
+                </div>
               </div>
-              <div>
-                <p>{booking.event_type}</p>
-                <p>{booking.start_time}</p>
-                <p>{booking.end_time}</p>
-                <p>{booking.paid}</p>
-                <p>{booking.total}</p>
-                <p>{booking.user.first_name}</p>
-                <p>{booking.user.last_name}</p>
-                <p>{booking.user.email}</p>
-                <p>{booking.user.phone_number}</p>
-              </div>
-              {/* <Link to={`/my-bookings/${performer.id}/${booking.id}`} className="btn btn-dark">
-                See Booking Details
-              </Link> */}
+            </div>
+            <div className="col-4">
+              <p>{booking.event_type}</p>
+              <p>{booking.start_time}</p>
+              <p>{booking.end_time}</p>
+              <p>{booking.paid}</p>
+              <p>{booking.total}</p>
+              <p>{booking.user.first_name}</p>
+              <p>{booking.user.last_name}</p>
+              <p>{booking.user.email}</p>
+              <p>{booking.user.phone_number}</p>
             </div>
           </div>
         </div>
