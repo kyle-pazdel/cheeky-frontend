a2299834154155c4b94bc0ac23a2873da0ec672e
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-02-04 22:30:36 -0800

    Adds file compression and acceptance of png and jpg to fileform component

--- src/FileForm.jsx
+++ src/FileForm.jsx
@@ -1,21 +1,46 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import imageCompression from "browser-image-compression";
 
 export function FileForm(props) {
   const [errors, setErrors] = useState([]);
   const [posts, setPosts] = useState([]);
   const [currentPost, setCurrentPost] = useState({});
 
-  const handleSubmit = (event) => {
+  // async function handleImageUpload(event) {
+  //   const imageFile = event.target.files[0];
+
+  //   const options = {
+  //     maxSizeMB: 0.08,
+  //   };
+  //   try {
+  //     const compressedFile = await imageCompression(imageFile, options);
+  //     console.log(compressedFile.size / 1024 / 1024);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   handleSubmit(event.target.title.value, compressedFile);
+  // }
+
+  async function handleSubmit(event) {
     event.preventDefault();
+    const imageFile = event.target.image.files[0];
+    const options = {
+      maxSizeMB: 0.1,
+    };
     const data = new FormData();
+    try {
+      const compressedFile = await imageCompression(imageFile, options);
+      data.append("post[image]", compressedFile);
+    } catch (error) {
+      console.log(error);
+    }
     data.append("post[performer_id]", props.performer.id);
     data.append("post[title]", event.target.title.value);
-    data.append("post[image]", event.target.image.files[0]);
     submitToAPI(data);
     event.target.reset();
     window.location.href = "/me";
-  };
+  }
   const submitToAPI = (data) => {
     axios
       .post(`/posts.json`, data)
@@ -51,7 +76,7 @@ export function FileForm(props) {
     <div>
       <p className="m-3 fs-4 fw-semibold card-title">Add Image</p>
       <p className="fs-6 fw-lighter">
-        File must be in <strong>jpeg</strong> format and cannot exceed <strong>100kb</strong>
+        File must be in <strong>jpeg</strong> / <strong>png</strong> format and cannot exceed <strong>1MB</strong>
       </p>
       <ul>
         {errors?.map((error) => (
@@ -60,7 +85,7 @@ export function FileForm(props) {
       </ul>
       <form className="mb-3" onSubmit={(event) => handleSubmit(event)}>
         <div className="mb-3">
-          <input className="form-control" type="file" accept="image/jpeg" name="image" id="formFileLg" />
+          <input className="form-control" type="file" accept="image/png, image/jpeg" name="image" id="formFileLg" />
         </div>
         <div className="input-group mb-3">
           <span className="input-group-text" id="basic-addon1">
