aea69368727763b8b09271f66190608c142f585e
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-02-06 11:35:45 -0800

    Adds responsive behaivor based on vp width for modal component

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,10 +1,22 @@
 import "./Modal.css";
+import useWindowDimensions from "./useWindowDimensions";
 
 export function Modal(props) {
+  const { height, width } = useWindowDimensions();
+  const handleModalSizing = () => {
+    if (width < 1000) {
+      return "modal-small";
+    } else if (width < 1200) {
+      return "modal-mid";
+    } else {
+      return "modal-main";
+    }
+  };
+
   if (props.show) {
     return (
       <div className="modal-background">
-        <section className="modal-main">
+        <section className={handleModalSizing()}>
           {props.children}
           <button className="close" type="button" onClick={props.onClose}>
             &#x2715;
